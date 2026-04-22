303373ea6bbaa3fe8f663795b98bc4a0fc6204fd
mandar kavthankar <nhanumak@gmail.com>
2026-04-22 13:12:46 +0530

    Commit Added ValuesController

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace React_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Default API Controller Newly Loaded");
+        }
+    }
+}
